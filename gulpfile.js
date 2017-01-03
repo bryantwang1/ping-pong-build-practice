0e7085a26a8cd92949e92f6f52cbf693c01667c4
Bryant Wang and Jesse McKay <jesse@>
2017-01-03 15:44:57 -0800

    finish bower lesson

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,20 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
+var browserSync = require('browser-sync').create();
+
+// var lib = require('bower-files')();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 
 var buildProduction = utilities.env.production;
 
@@ -41,13 +55,16 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
     .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task("build", ['clean'], function() {
+gulp.task('build', ['clean'], function(){
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
+  gulp.start('bower');
 });
+
+// Since we will always want to include our vendor files whether or not we are making a production build, we will just call the bower task using gulp.start at the end of our build task.
 
 gulp.task("clean", function() {
   return del(['build', 'tmp']);
@@ -59,6 +76,44 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('default'));
 });
 
-// running gulp jsBrowserify will tell the gulp task to run, which is
-// dependent on the concatInterface task running, so essentially they
-// both run.
+gulp.task('bowerJS', function() {
+  return gulp.src(lib.ext('js').files)
+  //targeting all js files
+    .pipe(concat('vendor.min.js'))
+    //concatenating them together
+    .pipe(uglify())
+    //minifiying further
+    .pipe(gulp.dest('./build/js'));
+    //new destination folder!
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+  .pipe(concat("vendor.css"))
+  .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+// This task has no callback function, but it has 2 dependency tasks: bowerJS and bowerCSS. Incidentally, it is important to note that the order of the tasks in the dependency array is ignored by gulp. So we can't use this method if we need one task to be completed before the next one in the array begins. Our bower task above will run both the bowerJS and bowerCSS tasks concurrently. This is nice because it is faster than running them one after the other! But if we need to run task1 before task2, then we must specify task1 as a dependency of task2, not just list them in order as part of a third master task.
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
+
+// We can keep adding as many watchers and build tasks as we need using this format. Just remember that any time you change a gulp task, you must restart the server.
