f53584578414b27091b4b08a4526923b1a18b2c3
Bryant Wang and Jesse McKay <bryant@>
2017-01-03 16:46:56 -0800

    final commit

--- build/js/app.js
+++ build/js/app.js
@@ -49,4 +49,16 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
+var apiKey = "3e6315e67187e13d82c40254eb4beece";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
+
 },{"./../js/pingpong.js":1}]},{},[2]);
---
+++ js/weather-interface.js
@@ -0,0 +1,11 @@
+var apiKey = "3e6315e67187e13d82c40254eb4beece";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
